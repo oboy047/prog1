57265c4d9f90c6f46b9e3004799c674d34bdaa17
ncwrab <143709905+ncwrab@users.noreply.github.com>
2023-09-01 14:30:01 +0200

    Add files via upload

---
+++ Forelesninger/Forelesning02 - variabler og datatyper/numbers.py
@@ -0,0 +1,30 @@
+
+
+variabel = 5            # Innholdet i variabelen er av datatypen int (heltall)
+print(variabel)
+
+
+# Hvilke verdier vil bli printet ut? 
+number = 2
+print(number)
+number = 6
+print(number)
+
+# Hva vil skje her?
+x, y = 12, 34.9
+print(x)
+print(y)
+
+# Hva vil skje her?
+a = 2
+b = 2
+print(a)
+a = a + b
+print(a)
+b = a + b
+print(b)
+
+# Hva blir utskriften her?
+dolphins_sleep_with_one_eye_open = False
+print(dolphins_sleep_with_one_eye_open)
+
---
+++ Forelesninger/Forelesning02 - variabler og datatyper/string.py
@@ -0,0 +1,19 @@
+#print("A string")
+
+#print('Also a string')
+
+print('We can put quotes inside a string: "First, solve the problem. Then, write the code." ')
+
+print("With apostrophe: It's harder to read code than to write it. - Joel Spolsky")
+
+print('''Why not both?: "It's harder to read code than to write it." - Joel Spolsky''')
+
+quote = "It's harder to read code than to write it."
+
+author = "Joel Spolsky"
+
+print(author)
+
+print('A quote: "', quote, '" - ', author )
+
+print('A quote: "' + quote, '" - ' + author)
